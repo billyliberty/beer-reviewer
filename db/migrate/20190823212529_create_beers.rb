6493af7db5d4c001f03e6e617664faf5029d6f09
billyliberty <joshua.crum@gmail.com>
2019-08-23 16:31:18 -0500

    Added preliminary databse categories.

--- db/migrate/20190823212529_create_beers.rb
+++ db/migrate/20190823212529_create_beers.rb
@@ -1,6 +1,9 @@
 class CreateBeers < ActiveRecord::Migration[5.2]
   def change
     create_table :beers do |t|
+      t.string :name
+      t.string :style
+      t.string :brewery
 
       t.timestamps
     end
