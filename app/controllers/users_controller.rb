2d5616238530b9957b41f6158b1ab716e43f7ab8
billyliberty <joshua.crum@gmail.com>
2019-08-23 18:29:35 -0500

    Added create action to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,17 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id 
+            redirect_to user_path(user)
+        else
+            render :new
+        end
     end
 
     def new
+        @user = User.new
     end
 
 end
