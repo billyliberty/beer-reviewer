51eae154f6a5161ab7e1c51d363e034110188d87
billyliberty <joshua.crum@gmail.com>
2019-08-24 13:03:00 -0500

    Updated BEERS controller actions.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,36 @@
 class BeersController < ApplicationController
+
+    def index
+        @beers = Beer.all 
+    end
+
+    def show
+        @beer = Beer.find_by(id: params[:id])
+    end
+
+    def create
+        @beer = Beer.find_or_create_by(beer_params)
+        if @beer
+            redirect_to beer_path(@beer)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @beer = Beer.find_by(id: params[:id])
+    end
+
+    def update
+        @beer = Beer.find_by(id: params[:id])
+        @beer.update(beer_params)
+        redirect_to @beer
+    end
+
+    private
+
+    def beer_params
+        params.require(:beer).permit(:name, :style, :brewery, :abv)
+    end
+
 end
