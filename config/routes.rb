8955aabbcb58c22be7784af9b1457b4328432a75
billyliberty <joshua.crum@gmail.com>
2019-08-24 12:43:45 -0500

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :users
   resources :reviews
   resources :beers
+
+  get '/signin', to: 'session#new'
+  post '/signin', to: 'session#create'
+  delete '/session', to: 'session#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
