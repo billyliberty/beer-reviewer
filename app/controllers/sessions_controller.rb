98136df350f5c6f0486b41a9700f94415aea2147
billyliberty <joshua.crum@gmail.com>
2019-08-24 12:27:51 -0500

    Added create action to SESSIONS controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(:email => params[:user][:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.user_id
+            redirect_to user_path(user)
+        else
+            render :new
+        end
+    end
 end
