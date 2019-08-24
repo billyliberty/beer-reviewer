0eee2ae2f90862387def200e23038c2106b6f0b5
billyliberty <joshua.crum@gmail.com>
2019-08-24 12:32:24 -0500

    Added helper methods for required login and check if user is logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
 
     def current_user
         if session[:user_id].present?
-            user = User.find_by(:id => sessions[:user_id])
+            user = User.find_by(:id => session[:user_id])
         end
+    end
+
+    def require_login
+        unless current_user
+            redirect_to root_path
+        end
+    end
+
+    def logged_in?
+        session[:user.id]
     end
 
 
