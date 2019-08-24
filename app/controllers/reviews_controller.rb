98b86067f7f2ed5b64a94e7d14342330630efff2
billyliberty <joshua.crum@gmail.com>
2019-08-24 14:38:11 -0500

    Added initial actions to reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,26 @@
 class ReviewsController < ApplicationController
 
     before_action :require_login
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :review, :price, :store, :date_of_purchase)
+    end
     
 end
