559ee00899de38f06777bff5329c637ca4022c09
billyliberty <joshua.crum@gmail.com>
2019-08-24 13:07:41 -0500

    Corrected routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get '/signin', to: 'session#new'
   post '/signin', to: 'session#create'
   delete '/session', to: 'session#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
