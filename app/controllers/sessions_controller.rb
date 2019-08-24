4b9bf94bf8dcb5302604b73d6fa5eadeb44b4619
billyliberty <joshua.crum@gmail.com>
2019-08-24 12:29:18 -0500

    Added destroy function for ending session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,10 @@ class SessionsController < ApplicationController
             render :new
         end
     end
+
+    def destroy
+        if current_user
+            session.delete = user.user_id
+            redirect_to root_path
+        end
 end
